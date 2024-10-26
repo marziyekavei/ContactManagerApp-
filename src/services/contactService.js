11b12bce6eb3082c791e2969a3c2b256156d4969
marziyekavei <138191022+marziyekavei@users.noreply.github.com>
2024-10-26 17:23:45 +0330

    change server

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:9000";
+const SERVER_URL = "https://contactmanagerserver.vercel.app";
 
 //@dec get all contact 
 //@route GET http://localhost:9000/contacts
